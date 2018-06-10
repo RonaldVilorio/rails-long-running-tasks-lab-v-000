81dea758e66f64fb36d3a01be6e13a1711ed7276
ronald vilorio <soccer23415@gmail.com>
2018-06-10 02:16:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,6 +45,7 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
   def upload
+    binding.pry
     CSV.foreach(params[:leads].path, headers: true) do |lead|
       
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
